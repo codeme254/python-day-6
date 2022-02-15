041081d93658b657ae70a9885e71344aa78d764f
otwoma <otwomadennis365@gmail.com>
2022-02-15 19:53:42 +0300

    initial commit

---
+++ learn.py
@@ -0,0 +1,17 @@
+#PYTHON FUNCTIONS
+# built in functions -> sum() int() str() input() print() len() etc
+# our own functions:
+def my_function():
+    print("Hello")
+    print("Bye")
+
+# triggering the function/ calling the function to do what we want
+my_function()
+
+# the while loop: the loop that continues going until a certain condition is met
+
+number_of_hurdles = 6
+while number_of_hurdles > 0:
+    print(f"Jumping hurdle round {number_of_hurdles}")
+    number_of_hurdles -= 1
+
